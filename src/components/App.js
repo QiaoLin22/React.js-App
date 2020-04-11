55d593f9021443f5c5c7aa67efe4ea2dff921853
QiaoLin22 <qiao.lin@emory.edu>
2020-04-10 21:13:40 -0400

    finish css

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import RecipeList from './RecipeList'
 import '../css/app.css'
+import uuidv4 from 'uuid/v4'
+import RecipeEdit from './RecipeEdit';
+
+export const RecipeContext = React.createContext()
+const LOCAL_STORRAEG_KEY = 'cookingWithReact.recipes'
 
 function App() {
-  return <RecipeList recipes = {sampleRecipes} />
+  const [recipes, setRecipes] = useState(sampleRecipes)
+
+  useEffect(() =>{
+    const recipeJSON = localStorage.getItem(LOCAL_STORRAEG_KEY)
+    if (recipeJSON != null) setRecipes(JSON.parse(recipeJSON))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem( LOCAL_STORRAEG_KEY, JSON.stringify(recipes))
+  }, [recipes])
+
+  const recipeContextValue = {
+    handleRecipeAdd: handleRecipeAdd,
+    handleRecipeDelete: handleRecipeDelete
+  }
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [{
+        id: uuidv4(), name: 'Name', amount: '1 Tbs'
+      }]
+    }
+  
+    setRecipes([...recipes, newRecipe])
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
+
+  return (
+    <RecipeContext.Provider value = {recipeContextValue} >
+      <RecipeList recipes = {recipes} />
+      <RecipeEdit/>
+    </RecipeContext.Provider>
+  )
+
+  
 }
+
+
 
 const sampleRecipes = [
   {
